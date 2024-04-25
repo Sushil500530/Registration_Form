f78bce709000c5ffce3ccca091edc7e4b794c54c
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-25 22:35:02 +0600

    add form validation and customize other

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,28 +1,67 @@
 import { useContext, useState } from "react";
 import "./register.css"
 import { AuthContext } from "../../providers/AuthProvider";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 
 const Register = () => {
-    const { createUser,upadatedProfile } = useContext(AuthContext);
+    const { createUser, upadatedProfile } = useContext(AuthContext);
+    const navigate = useNavigate();
     const [user, setUser] = useState({
         username: "",
         email: "",
         password: ""
     })
-    const handleResister = async (user) => {
-        
-        createUser(user?.email, user?.password)
-            .then((result) => {
-                if (result?.user) {
-                    upadatedProfile(user?.username)
-                        .then(() => { })
-                    toast.success('Login Successful')
-                    navigate("/")
-                }
-            })
-            .catch(err => toast.error(err.message))
+    const name = user.username?.length;
+    const emai = user.email?.length;
+    const pass = user.password?.length;
+
+    const handleResister = async () => {
+        const emailVerification = (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(user?.email))
+        if (name > 0 && emai > 0 && pass > 0) {
+            if (emailVerification === true) {
+
+                return await createUser(user?.email, user?.password)
+                    .then((result) => {
+                        if (result?.user) {
+                            upadatedProfile(user?.username)
+                                .then(() => { })
+                            console.log(result?.user?.displayName);
+                            console.log(result?.user?.email);
+                            console.log(result?.user?.emailVerified);
+                            toast.success('login successfully....!');
+                            // navigate("/")
+                        }
+                    })
+                    .catch(err => toast.error(err.message))
+            } else {
+
+                return toast.error("please enter your valid email.....!")
+            }
+
+        }
+        return toast.error("Please fill in the all input field...!")
+        // console.log(emailVerification, "-----?> value");
+        // console.log(name, "-----?> value");
+
+
+
+
+        //  toast.error("You have entered an invalid email address!")
+
+        //   await createUser(user?.email, user?.password)
+        //         .then((result) => {
+        //             if (result?.user) {
+        //                upadatedProfile(user?.username)
+        //                     .then(() => { })
+        //                 toast.success('Login Successful')
+        //                 // navigate("/")
+        //             }
+        //         })
+        //         .catch(err => toast.error(err.message))
     }
+
     return (
         <div className="register-header">
             <h1>Register Now!</h1>
@@ -39,8 +78,8 @@ const Register = () => {
                     <label htmlFor="username">Password*</label> <br />
                     <input type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="Enter your password..." />
                 </div>
-                <div onClick={handleResister} className="btn-content btn">
-                    <button className="">Submit</button>
+                <div className="btn-content">
+                    <button onClick={handleResister} className="btn">Submit</button>
                 </div>
             </div>
         </div>
--- src/providers/firebase/firebase.config.js
+++ src/providers/firebase/firebase.config.js
@@ -1,21 +1,39 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey:import.meta.env.VITE_APIKEY ,
+//     authDomain:import.meta.env.VITE_AUTHDOMAIN ,
+//     projectId:import.meta.env.VITE_PROJECTID ,
+//     storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
+//     messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+//     appId:import.meta.env.VITE_APPID
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// export default auth;
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey:import.meta.env.VITE_APIKEY ,
-    authDomain:import.meta.env.VITE_AUTHDOMAIN ,
-    projectId:import.meta.env.VITE_PROJECTID ,
-    storageBucket:import.meta.env.VITE_STORAGEBUCKET ,
-    messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
-    appId:import.meta.env.VITE_APPID
+    apiKey: "AIzaSyB4EbFoLMYnUDZruK4Li8HjNL1D0lQOe90",
+    authDomain: "registrationform-385ff.firebaseapp.com",
+    projectId: "registrationform-385ff",
+    storageBucket: "registrationform-385ff.appspot.com",
+    messagingSenderId: "276964877164",
+    appId: "1:276964877164:web:328dae0c7355c0cbac0949"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
-
