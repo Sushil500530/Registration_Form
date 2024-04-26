89a00bb4c7514120efb381ae47a952b8f1e0338a
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-26 10:54:12 +0600

    add spinner when click the login button and other

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Home = () => {
+    const {user} = useContext(AuthContext);
+    console.log(user?.displayName);
     return (
         <div className="top">
             <h1>All Show Here.....!</h1>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,10 +3,12 @@ import "./register.css"
 import { AuthContext } from "../../providers/AuthProvider";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { FaSpinner } from "react-icons/fa";
 
 
 const Register = () => {
     const { createUser, upadatedProfile } = useContext(AuthContext);
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
     const [user, setUser] = useState({
         firstname: "",
@@ -14,47 +16,55 @@ const Register = () => {
         email: "",
         contact: "",
         password: "",
-        confirmpassword:''           
+        confirmpassword: ''
 
     })
-    console.log(user);
+
     const fname = user.firstname?.length;
     const lname = user.lastname?.length;
     const emai = user.email?.length;
     const contactNumber = user.contact?.length;
     const pass = user.password?.length;
     const confirmpass = user.password?.length;
-    const username =user.firstname+ " " + user.lastname;
-    console.log(username);
+    const username = user.firstname + " " + user.lastname;
 
     const handleResister = async () => {
+        setIsLoading(true)
         const emailVerification = (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(user?.email))
         if (fname > 0 && lname > 0 && contactNumber > 0 && emai > 0 && pass > 0 && confirmpass > 0) {
             if (emailVerification === true) {
-                if(user.password === user.confirmpassword){
-                 return await createUser(user?.email, user?.password)
-                    .then((result) => {
-                        if (result?.user) {
-                            upadatedProfile(username)
-                                .then(() => { })
-                            console.log(result?.user?.displayName);
-                            console.log(result?.user?.email);
-                            console.log(result?.user?.emailVerified);
-                            toast.success('login successfully....!');
-                            navigate("/")
-                        }
-                    })
-                    .catch(err => toast.error(err.message))
-                } else{
+                if (user.password === user.confirmpassword) {
+                    return await createUser(user?.email, user?.password)
+                        .then((result) => {
+                            if (result?.user) {
+                                upadatedProfile(username)
+                                    .then(() => { })
+                                console.log(result?.user);
+                                // console.log(result?.user?.displayName);
+                                // console.log(result?.user?.email);
+                                // console.log(result?.user?.emailVerified);
+                                console.log(user);
+                                toast.success('login successfully....!');
+                                setIsLoading(false)
+                                navigate("/")
+                            }
+                        })
+                        .catch(err => {
+                            setIsLoading(false)
+                            toast.error(err.message)
+                        })
+                } else {
+                    setIsLoading(false)
                     return toast.error("password doesn't match! please try again....!")
                 }
-                
-            } else {
 
+            } else {
+                setIsLoading(false)
                 return toast.error("please enter your valid email.....!")
             }
 
         }
+        setIsLoading(false)
         return toast.error("Please fill in the all input field...!")
     }
 
@@ -87,7 +97,15 @@ const Register = () => {
                     <input type="password" value={user.confirmpassword} onChange={(e) => setUser({ ...user, confirmpassword: e.target.value })} placeholder="Enter your password..." />
                 </div>
                 <div className="btn-content">
-                    <button onClick={handleResister} className="btn">Submit</button>
+                    <button onClick={handleResister} className="btn">
+                        {
+                            isLoading ?  (
+                                <span className='spinner-header flex items-center justify-center gap-3'> <FaSpinner className='m-auto animate' size={24} /> Processing....</span>
+                            ) : (
+                                'Register'
+                            )
+                        }
+                    </button>
                 </div>
             </div>
         </div>
