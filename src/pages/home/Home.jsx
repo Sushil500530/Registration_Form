7ebc8047d553bd279c9957fd45cc09d139b8debc
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-26 15:14:56 +0600

    add readme file some text

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+    const navigate = useNavigate();
     const {user} = useContext(AuthContext);
-    console.log(user?.displayName);
+    // console.log(user);
+    if(!user){
+        return navigate('/register')
+    }
     return (
         <div className="top">
             <h1>All Show Here.....!</h1>
