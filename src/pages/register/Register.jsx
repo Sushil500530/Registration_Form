90cf027958570a669f8c225e507cbc75808a71f7
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-04-26 10:19:42 +0600

    add update register page and update other

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -9,32 +9,46 @@ const Register = () => {
     const { createUser, upadatedProfile } = useContext(AuthContext);
     const navigate = useNavigate();
     const [user, setUser] = useState({
-        username: "",
+        firstname: "",
+        lastname: "",
         email: "",
-        password: ""
+        contact: "",
+        password: "",
+        confirmpassword:''           
+
     })
-    const name = user.username?.length;
+    console.log(user);
+    const fname = user.firstname?.length;
+    const lname = user.lastname?.length;
     const emai = user.email?.length;
+    const contactNumber = user.contact?.length;
     const pass = user.password?.length;
+    const confirmpass = user.password?.length;
+    const username =user.firstname+ " " + user.lastname;
+    console.log(username);
 
     const handleResister = async () => {
         const emailVerification = (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(user?.email))
-        if (name > 0 && emai > 0 && pass > 0) {
+        if (fname > 0 && lname > 0 && contactNumber > 0 && emai > 0 && pass > 0 && confirmpass > 0) {
             if (emailVerification === true) {
-
-                return await createUser(user?.email, user?.password)
+                if(user.password === user.confirmpassword){
+                 return await createUser(user?.email, user?.password)
                     .then((result) => {
                         if (result?.user) {
-                            upadatedProfile(user?.username)
+                            upadatedProfile(username)
                                 .then(() => { })
                             console.log(result?.user?.displayName);
                             console.log(result?.user?.email);
                             console.log(result?.user?.emailVerified);
                             toast.success('login successfully....!');
-                            // navigate("/")
+                            navigate("/")
                         }
                     })
                     .catch(err => toast.error(err.message))
+                } else{
+                    return toast.error("password doesn't match! please try again....!")
+                }
+                
             } else {
 
                 return toast.error("please enter your valid email.....!")
@@ -42,24 +56,6 @@ const Register = () => {
 
         }
         return toast.error("Please fill in the all input field...!")
-        // console.log(emailVerification, "-----?> value");
-        // console.log(name, "-----?> value");
-
-
-
-
-        //  toast.error("You have entered an invalid email address!")
-
-        //   await createUser(user?.email, user?.password)
-        //         .then((result) => {
-        //             if (result?.user) {
-        //                upadatedProfile(user?.username)
-        //                     .then(() => { })
-        //                 toast.success('Login Successful')
-        //                 // navigate("/")
-        //             }
-        //         })
-        //         .catch(err => toast.error(err.message))
     }
 
     return (
@@ -67,16 +63,28 @@ const Register = () => {
             <h1>Register Now!</h1>
             <div className="register-content">
                 <div className="content">
-                    <label htmlFor="username">Username*</label> <br />
-                    <input type="text" value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} placeholder="Enter your username..." />
+                    <label htmlFor="firstname">First Name*</label> <br />
+                    <input type="text" value={user.firstname} onChange={(e) => setUser({ ...user, firstname: e.target.value })} placeholder="Enter your firstname..." />
+                </div>
+                <div className="content">
+                    <label htmlFor="lastname">Last Name*</label> <br />
+                    <input type="text" value={user.lastname} onChange={(e) => setUser({ ...user, lastname: e.target.value })} placeholder="Enter your lastname..." />
                 </div>
                 <div className="content">
                     <label htmlFor="email">Email*</label> <br />
                     <input type="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="Enter your email..." />
                 </div>
                 <div className="content">
-                    <label htmlFor="username">Password*</label> <br />
+                    <label htmlFor="contact">Contact*</label> <br />
+                    <input type="number" value={user.contact} onChange={(e) => setUser({ ...user, contact: e.target.value })} placeholder="Enter your contact..." />
+                </div>
+                <div className="content">
+                    <label htmlFor="password">Password*</label> <br />
                     <input type="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="Enter your password..." />
+                </div>
+                <div className="content">
+                    <label htmlFor="confirmpassword">Confirm Password*</label> <br />
+                    <input type="password" value={user.confirmpassword} onChange={(e) => setUser({ ...user, confirmpassword: e.target.value })} placeholder="Enter your password..." />
                 </div>
                 <div className="btn-content">
                     <button onClick={handleResister} className="btn">Submit</button>
